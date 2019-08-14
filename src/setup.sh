cb77a39b2850eabc9030eb29f9875c2991455042
Patrik Herrgård <patrik.herrgard@vincit.fi>
2019-08-14 09:58:00 +0300

    Added Java stuff to brewfile

--- src/setup.sh
+++ src/setup.sh
@@ -7,3 +7,6 @@ brew bundle
 curl -O https://raw.githubusercontent.com/MartinSeeler/iterm2-material-design/master/material-design-colors.itermcolors
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl https://raw.githubusercontent.com/github/gitignore/master/Global/macOS.gitignore -o ~/.gitignore
+
+jenv add /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+jenv add /Library/Java/JavaVirtualMachines/openjdk-12.0.2.jdk/Contents/Home
